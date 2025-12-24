39d3ca384ba18e059e83d4b927cef25e4e3ad7aa
Chanel Chinedu <LongLiveTheKing777@protonmail.com>
2025-12-24 13:58:46 +0000

    Plugged the typewriter library into my code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Challenge 2
+let challenge2 = document.querySelector("#challenge-2");
+challenge2.innerHTML = "Chanel Obinwanne Chinedu";
+
+// Challenge 3
+//challenge3 = document.querySelector("#challenge-3");
+new Typewriter("#challenge-3", {
+  strings: "Chanel Obinwanne Chinedu",
+  autoStart: true,
+});
